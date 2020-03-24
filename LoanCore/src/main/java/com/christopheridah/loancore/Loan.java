873c963166579afd3d1d10c54a1cc7494bd39a5a
ChrisOgal <42741822+ChrisOgal@users.noreply.github.com>
2020-03-23 20:50:55 -0400

    Loan Core
    Loan and Member Entity Classes

---
+++ LoanCore/src/main/java/com/christopheridah/loancore/Loan.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.christopheridah.loancore;
+
+/**
+ *
+ * @author chris
+ */
+public class Loan {
+    
+    private int id;
+    private Member borrower;
+    private String bookTitle, borrowDate, returnDate;
+    
+    public Loan () {
+        
+    }
+
+    public Loan(int id, Member borrower, String bookTitle, String borrowDate, String returnDate) {
+        this.id = id;
+        this.borrower = borrower;
+        this.bookTitle = bookTitle;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Member getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Member borrower) {
+        this.borrower = borrower;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(String borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+    
+    
+    
+}
